6c1054b87e31eb471e5827cc92982f7953592e2d
shagar619 <ashagar619@gmail.com>
2025-12-14 23:38:10 +0600

    added svg path

--- src/components/modules/Home/Hero.tsx
+++ src/components/modules/Home/Hero.tsx
@@ -1,4 +1,6 @@
 import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
 import { HeroProps } from "@/types/heroProps";
 
 

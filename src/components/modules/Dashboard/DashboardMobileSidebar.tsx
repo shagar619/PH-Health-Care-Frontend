0aabb23b2bfe2053c77d30ba5b8df194c10e664d
shagar619 <ashagar619@gmail.com>
2025-12-21 18:23:09 +0600

    added scroll areana for navbar

--- src/components/modules/Dashboard/DashboardMobileSidebar.tsx
+++ src/components/modules/Dashboard/DashboardMobileSidebar.tsx
@@ -1,4 +1,15 @@
+"use client";
 
+import { Badge } from "@/components/ui/badge";
+import { ScrollArea } from "@/components/ui/scroll-area";
+import { Separator } from "@/components/ui/separator";
+import { SheetTitle } from "@/components/ui/sheet";
+import { getIconComponent } from "@/lib/icon-mapper";
+import { cn } from "@/lib/utils";
+import { NavSection } from "@/types/dashboard.interface";
+import { UserInfo } from "@/types/user.interface";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
 
 interface DashboardMobileSidebarContentProps {
   userInfo: UserInfo;

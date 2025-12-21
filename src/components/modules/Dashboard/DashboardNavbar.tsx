6e2615b1d0fcbe3012c493ca2b64d3b4407d3e22
shagar619 <ashagar619@gmail.com>
2025-12-21 17:57:32 +0600

    added drop down menu items

--- src/components/modules/Dashboard/DashboardNavbar.tsx
+++ src/components/modules/Dashboard/DashboardNavbar.tsx
@@ -1,12 +1,16 @@
 import { getUserInfo } from "@/services/auth/getUserInfo";
 import DashboardNavbarContent from "./DashboardNavbarContent";
 import { UserInfo } from "@/types/user.interface";
+import { getNavItemsByRole } from "@/lib/navItems.config";
+import { getDefaultDashboardRoute } from "@/lib/auth-utils";
 
 
 const DashboardNavbar = async () => {
 
      const userInfo = (await getUserInfo()) as UserInfo;
+
      const navItems = getNavItemsByRole(userInfo.role);
+
      const dashboardHome = getDefaultDashboardRoute(userInfo.role);
 
      return (

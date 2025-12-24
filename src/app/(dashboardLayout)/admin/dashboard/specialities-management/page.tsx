4785754024213b4e83c54af7c7b857f3b4e44f45
shagar619 <ashagar619@gmail.com>
2025-12-24 15:56:47 +0600

    added Specialities Management Table

--- src/app/(dashboardLayout)/admin/dashboard/specialities-management/page.tsx
+++ src/app/(dashboardLayout)/admin/dashboard/specialities-management/page.tsx
@@ -1,8 +1,24 @@
+import SpecialitiesManagementHeader from "@/components/modules/Admin/SpecialitiesManagement/SpecialitiesManagementHeader";
+import RefreshButton from "@/components/shared/RefreshButton";
+import { TableSkeleton } from "@/components/shared/TableSkeleton";
+import { Suspense } from "react";
 
 
-const AdminSpecialitiesManagementPage = () => {
+const AdminSpecialitiesManagementPage = async () => {
 
-     return <div>AdminSchedulesManagementPage</div>;
+     // const result = await getSpecialities();
+
+     return (
+     <div className="space-y-6">
+          {/* <SpecialitiesManagementHeader /> */}
+     <div className="flex">
+          <RefreshButton />
+     </div>
+     {/* <Suspense fallback={<TableSkeleton columns={2} rows={10} />}>
+          <SpecialitiesTable specialities={result.data} />
+     </Suspense> */}
+     </div>
+);
 };
 
 export default AdminSpecialitiesManagementPage;

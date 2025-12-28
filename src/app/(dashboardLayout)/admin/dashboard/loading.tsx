4702508fe91538a896d78e0c9333ae76d8585df7
shagar619 <ashagar619@gmail.com>
2025-12-28 21:14:42 +0600

    added dashboard skeleton

---
+++ src/app/(dashboardLayout)/admin/dashboard/loading.tsx
@@ -0,0 +1,8 @@
+import { DashboardSkeleton } from "@/components/shared/DashboardSkeleton";
+
+
+const AdminDashboardLoading = () => {
+     return <DashboardSkeleton />;
+};
+
+export default AdminDashboardLoading;

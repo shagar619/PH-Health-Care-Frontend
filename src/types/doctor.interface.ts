af782732fb5c0d575382339579c8a1390cb37e6b
shagar619 <ashagar619@gmail.com>
2025-12-31 22:54:40 +0600

    added useSpecialtySelection Hook For Multi Select Specialty Component

--- src/types/doctor.interface.ts
+++ src/types/doctor.interface.ts
@@ -20,6 +20,7 @@ export interface IDoctor {
      createdAt?: string;
      updatedAt?: string;
      doctorSpecialties?: Array<{
+          specialitiesId?: string;
           specialties?: {
                id: string;
                title: string;

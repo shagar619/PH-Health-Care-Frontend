19f92e39ab016d905e20c4d999d29a2208beeb7d
shagar619 <ashagar619@gmail.com>
2026-01-01 00:32:18 +0600

    added filter doctor data

--- src/app/(dashboardLayout)/admin/dashboard/doctors-management/page.tsx
+++ src/app/(dashboardLayout)/admin/dashboard/doctors-management/page.tsx
@@ -1,14 +1,11 @@
+import DoctorFilters from "@/components/modules/Admin/DoctorsManagement/DoctorFilters";
 import DoctorsManagementHeader from "@/components/modules/Admin/DoctorsManagement/DoctorsManagementHeader";
 import DoctorsTable from "@/components/modules/Admin/DoctorsManagement/DoctorsTable";
-import RefreshButton from "@/components/shared/RefreshButton";
-import SearchFilter from "@/components/shared/SearchFilter";
-import SelectFilter from "@/components/shared/SelectFilter";
 import TablePagination from "@/components/shared/TablePagination";
 import { TableSkeleton } from "@/components/shared/TableSkeleton";
 import { queryStringFormatter } from "@/lib/formatters";
 import { getDoctors } from "@/services/admin/doctorManagement";
 import { getSpecialities } from "@/services/admin/specialitiesManagement";
-import { ISpecialty } from "@/types/specialities.interface";
 import { Suspense } from "react";
 
 
@@ -29,33 +26,22 @@ const AdminDoctorsManagementPage = async ({
      const doctorsResult = await getDoctors(queryString);
 
      const totalPages = Math.ceil(
-     doctorsResult.meta.total / doctorsResult.meta.limit
+     doctorsResult?.meta?.total || 1 / doctorsResult?.meta?.limit || 1
 );
 
 
      return (
      <div className="space-y-6">
-     <DoctorsManagementHeader specialities={specialitiesResult.data} />
-     <div className="flex space-x-2">
-     <SearchFilter paramName="searchTerm" placeholder="Search doctors..." />
-     <SelectFilter
-          paramName="speciality" // ?speciality="Cardiology"
-          options={specialitiesResult.data.map((speciality: ISpecialty) => ({
-               label: speciality.title,
-               value: speciality.title,
-          }))}
-          placeholder="Filter by speciality"
-     />
-     <RefreshButton />
-     </div>
+     <DoctorsManagementHeader specialities={specialitiesResult?.data || []} />
+     <DoctorFilters specialties={specialitiesResult?.data || []} />
      <Suspense fallback={<TableSkeleton columns={10} rows={10} />}>
      <DoctorsTable
           doctors={doctorsResult.data}
-          specialities={specialitiesResult.data}
+          specialities={specialitiesResult?.data || []}
      />
      <TablePagination
-          currentPage={doctorsResult.meta.page}
-          totalPages={totalPages}
+          currentPage={doctorsResult?.meta?.page || 1}
+          totalPages={totalPages || 1}
      />
      </Suspense>
      </div>

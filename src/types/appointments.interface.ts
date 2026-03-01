38bd22f109130de28702471b2b48c3f7996c62d7
shagar619 <ashagar619@gmail.com>
2026-03-01 22:21:21 +0600

    added type of payment gateway

--- src/types/appointments.interface.ts
+++ src/types/appointments.interface.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import { IDoctor } from "./doctor.interface";
 import { IPatient } from "./patient.interface";
 import { IPrescription } from "./prescription.interface";
@@ -35,6 +36,21 @@ export interface IAppointment {
      review?: IReview;
      // payment?: IPayment;
 }
+
+
+export interface IPayment {
+     id: string;
+     appointmentId: string;
+     amount: number;
+     transactionId: string;
+     status: PaymentStatus;
+     paymentGatewayData?: any;
+     stripeEventId?: string;
+
+     createdAt: string;
+     updatedAt: string;
+}
+
 
 export interface IAppointmentFormData {
      doctorId: string;
